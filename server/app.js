16c690976f2cb898001ecdf1e70e731677ede49e
Anthony Kimble <anthony.kimble@atlasscool.com>
2025-07-15 17:38:04 -0500

    root query

--- server/app.js
+++ server/app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
-const schema = require('./schema/schema'); // Import the schema
+// Require the schema we just created
+const schema = require('./schema/schema');
 
 const app = express();
 
-// Set up the GraphQL endpoint
 app.use('/graphql', graphqlHTTP({
-  schema: schema, // Pass the schema to the middleware
-  graphiql: true  // Enable the GraphiQL tool for easy testing in the browser
+  // Pass the schema to the middleware
+  schema: schema,
+  // Enable GraphiQL for in-browser testing
+  graphiql: true
 }));
 
 app.listen(4000, () => {
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,40 +2,38 @@ const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
-  GraphQLSchema,
-  GraphQLID // Using GraphQLID is a best practice for unique identifiers
+  GraphQLSchema // GraphQLSchema is needed to create the schema
 } = require('graphql');
 
-// Define the Task Type
+// 1. Define the Task Type
 const TaskType = new GraphQLObjectType({
   name: 'Task',
   fields: () => ({
-    id: { type: GraphQLID },
+    id: { type: GraphQLString },
     title: { type: GraphQLString },
     weight: { type: GraphQLInt },
     description: { type: GraphQLString }
   })
 });
 
-// Define the Root Query
-// A schema must have a root query, even if it's empty initially.
-// This defines how users can query for data.
+// 2. Create the Root Query
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
     task: {
       type: TaskType,
-      args: { id: { type: GraphQLID } },
+      // Arguments required to query for a specific task
+      args: { id: { type: GraphQLString } },
+      // The function that retrieves the data
       resolve(parent, args) {
-        // This is where you would add logic to find and return a task from the database.
-        // For now, we'll return null.
-        return null;
+        // Code to get data from db / other source will go here.
+        // args.id will contain the ID passed by the client.
       }
     }
   }
 });
 
-// Create and export the schema
+// 3. Export the schema with the RootQuery
 module.exports = new GraphQLSchema({
   query: RootQuery
 });

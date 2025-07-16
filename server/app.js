41cbafa58a5b381c6eae35f7d78248477ca2a246
Anthony Kimble <anthony.kimble@atlasscool.com>
2025-07-16 13:14:17 -0500

    app file

--- server/app.js
+++ server/app.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose'); // Require Mongoose
 
 const app = express();
 
+// --- IMPORTANT ---
+// Replace the string below with your actual connection string from MongoDB Atlas
+const MONGO_URI = "mongodb+srv://<username>:<password>@cluster0.xxxxx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+// Connect to MongoDB Atlas database
+mongoose.connect(MONGO_URI);
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
+
+
 app.use('/graphql', graphqlHTTP({
   schema: schema,
-  // Enable GraphiQL for in-browser testing
   graphiql: true
 }));
 
